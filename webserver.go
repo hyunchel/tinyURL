50fc9d52b90b866746dcea65d3c338f926289184
Hyunchel Kim <hyunchel.inbox@gmail.com>
2017-09-24 18:33:43 -0500

    Use HTML tag when providing a link

--- webserver.go
+++ webserver.go
@@ -2,27 +2,26 @@ package tinyURL
 
 import (
 	"fmt"
+	"html"
 	"log"
 	"net/http"
 	"time"
 	"regexp"
 )
 
-func shortenUrl(originalUrl string) string {
-	url := HashIn(originalUrl)
+func shortenURL(originalURL string) string {
+	url := HashIn(originalURL)
 	if len(url) == 0 {
-		// Throw an Error here.
-		log.Printf("Unable to hash %q", originalUrl)
+		log.Printf("Unable to hash %q", originalURL)
 		return ""
 	}
 	return url
 }
 
-func lookupForUrl(shortenedUrl string) string {
-	PrintHashMapper()
-	url := HashOut(shortenedUrl)
+func lookupForURL(shortenedURL string) string {
+	url := HashOut(shortenedURL)
 	if len(url) == 0 {
-		log.Printf("Tried to HashOut %q, but got %q", shortenedUrl, url)
+		log.Printf("Tried to HashOut %q, but got %q", shortenedURL, url)
 		log.Println("Should throw 404.")
 		return ""
 	}
@@ -39,18 +38,16 @@ func CreateAndRunServer() {
 		case shortenRegex.MatchString(r.URL.Path):
 			r.ParseForm()
 			var message string
-			if longUrl := r.Form["url"]; longUrl != nil {
-				if shortenedUrl := shortenUrl(longUrl[0]); shortenedUrl != "" {
-					message = `
-						Your shortened URL:
-						%v
-					`
-					fmt.Fprintf(w, message, shortenedUrl)
+			if longURL := r.Form["url"]; longURL != nil {
+				if shortenedURL := shortenURL(longURL[0]); shortenedURL != "" {
+					resultURL := fmt.Sprintf("%v/redirect/%v", r.Host, shortenedURL)
+					message := fmt.Sprintf("<p>Your shortened URL:</p><a href=%q>%v</a>", resultURL, resultURL)
+					fmt.Fprintf(w, html.UnescapeString(message))
 				} else {
 					message = `
 						Sorry, failed to shorten %q
 					`
-					http.Error(w, fmt.Sprintf(message, longUrl), 400)
+					http.Error(w, fmt.Sprintf(message, longURL), 400)
 				}
 			} else {
 				message = `
@@ -60,16 +57,16 @@ func CreateAndRunServer() {
 			}
 		case redirectRegex.MatchString(r.URL.Path):
 			urlPath := redirectRegex.Split(r.URL.Path, 2)
-			originalUrl := lookupForUrl(urlPath[1])
+			originalURL := lookupForURL(urlPath[1])
 			code := 301
-			if originalUrl == "" {
+			if originalURL == "" {
 				http.NotFound(w, r)
 			}
-			http.Redirect(w, r, originalUrl, code)
+			http.Redirect(w, r, originalURL, code)
 		default:
 			// FIXME: Use links.
 			welcomeMessage := `
-				Welcome to tinyUrl.
+				Welcome to tinyURL.
 				In order to shorten, please head over to %q.
 				To use the shortened URL, head over to %q.
 			`
@@ -78,11 +75,11 @@ func CreateAndRunServer() {
 	})
 
 	s := &http.Server{
-		Addr:	":8080",
+		Addr:			":8080",
 		ReadTimeout:	10 * time.Second,
 		WriteTimeout:	10 * time.Second,
 		MaxHeaderBytes:	1 << 20,
 	}
-	log.Println("Listening at port 8080.")
+	log.Println("Listening at port 8080...")
 	log.Fatal(s.ListenAndServe())
 }
